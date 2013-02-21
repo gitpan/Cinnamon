0dd056e9e83d6f1e47f115e2abe8e3fd3e49ccb5
Yuki Shibazaki <shibayu36@gmail.com>
2013-02-21 15:44:13 +0000

    Import of SHIBAZAKI/Cinnamon-0.06 from CPAN.
    gitpan-cpan-distribution: Cinnamon
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SHIBAZAKI/Cinnamon-0.06.tar.gz
    gitpan-cpan-author:       SHIBAZAKI
    gitpan-cpan-maturity:     released

--- lib/Cinnamon.pm
+++ lib/Cinnamon.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010_001;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use YAML ();
 use Class::Load ();
@@ -62,6 +62,8 @@ sub run {
         "[error]: %s",
         (join(', ', @error)   || ''),
     );
+
+    return (\@success, \@error);
 }
 
 !!1;
